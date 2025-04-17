2d6609e9107d68e112ce0d5e633d8caa7a2f7875
CS-CL6\cs-yuasa <yuasa@comster.co.jp>
2025-04-17 12:19:31 +0900

    Mediatorパターンログ追加

--- src/main/java/jp/co/comster/mediator/MediatorMain.java
+++ src/main/java/jp/co/comster/mediator/MediatorMain.java
@@ -1,9 +1,12 @@
 package jp.co.comster.mediator;
 
+import org.tinylog.Logger;
+
 import jp.co.comster.mediator.framework.LoginFrame;
 
 public class MediatorMain {
 	static public void main(String[] args) {
+		Logger.info("Mediatorパターン開始");
 		new LoginFrame("Mediator Sample");
 	}
 }
